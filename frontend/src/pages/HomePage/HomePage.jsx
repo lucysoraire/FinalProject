b97cc953eeb6ec13c7802cb9cdfe81ef520f372b
naahuel-11 <nahuelromano911@gmail.com>
2025-04-30 00:22:38 -0300

    se agrego una funcion al home para que verifique si la cuenta esta iniciada o no para mostrar el boton de solicitar turno

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -23,28 +23,35 @@ import prevencion from "./../../assets/prevencion.png";
 import subsidio from "./../../assets/subsidio.png";
 import { ReactGoogleReviews } from "react-google-reviews";
 import "react-google-reviews/dist/index.css";
+
+
 const HomePage = () => {
   const featurableWidgetId = "05c2bf91-e5c4-4b8f-872d-f05e7e70d502";
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <section className="container-principal">
-    <div className="home">
-      <div className="titulovideo">
-        <video src={Intro} className="videointro" autoPlay loop />
-        <div className="intro">
-          <h1 className="fisiosport">FISIO SPORT</h1>
-          <h2 className="kinesiologiayfisioterapia">Kinesiología & Fisioterapia</h2>
-          <i><h3 className="kinesiologo">Klgo. Christian Núñez Coso</h3></i>
-          <Link to="/login">
-            <button className="button">
-              <span className="button-content">Solicitar turno</span>
-            </button>
-          </Link>
+      <div className="home">
+        <div className="titulovideo">
+          <video src={Intro} className="videointro" autoPlay loop />
+          <div className="intro">
+            <h1 className="fisiosport">FISIO SPORT</h1>
+            <h2 className="kinesiologiayfisioterapia">
+              Kinesiología & Fisioterapia
+            </h2>
+            <i>
+              <h3 className="kinesiologo">Klgo. Christian Núñez Coso</h3>
+            </i>
+            {!user && (
+              <Link to="/login">
+                <button className="button">
+                  <span className="button-content">Solicitar turno</span>
+                </button>
+              </Link>
+            )}
+          </div>
         </div>
-
       </div>
-
-    </div>
-        <div class="logos">
+      <div class="logos">
         <div class="logos-slide">
           <img src={osde} />
           <img src={iosfa} />
@@ -87,38 +94,42 @@ const HomePage = () => {
           <img src={prevencion} />
           <img src={subsidio} />
         </div>
-
       </div>
       <section className="about-us">
-      <div className="about-us-container">
-        <div className="about-us-image">
-        <img src={kinechris} alt="Kinesiologo Christian Nuñez Coso" />
+        <div className="about-us-container">
+          <div className="about-us-image">
+            <img src={kinechris} alt="Kinesiologo Christian Nuñez Coso" />
+          </div>
+          <div className="about-us-text">
+            <h2>HOLA</h2>
+            <p>
+              Soy Christian, kinesiólogo especializado en lesiones deportivas,
+              con un postgrado en Deportología de Alto Rendimiento. Desde 2016,
+              me he enfocado en la prevención y tratamiento de lesiones,
+              trabajando con equipos de alto rendimiento, incluyendo el plantel
+              superior de rugby de Club Cardenales desde hace más de 6 años. En
+              2020, abrí mi propio consultorio en Parque Centenario, brindando
+              atención personalizada de calidad.
+            </p>
+            <Link to="/turno">
+              <button className="buttonn" id="contactar">
+                <span>Contactar</span>
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="about-us-text">
-          <h2>HOLA</h2>
-          <p>
-          Soy Christian, kinesiólogo especializado en lesiones deportivas, con un postgrado en Deportología de Alto Rendimiento. Desde 2016, me he enfocado en la prevención y tratamiento de lesiones, trabajando con equipos de alto rendimiento, incluyendo el plantel superior de rugby de Club Cardenales desde hace más de 6 años. En 2020, abrí mi propio consultorio en Parque Centenario, brindando atención personalizada de calidad.
-
-
-          </p>
-          <Link to="/turno">
-            <button className="buttonn" id="contactar">
-              <span>Contactar</span>
-            </button>
-          </Link>
-        </div>
-        
+      </section>
+      <div className="reviews">
+        <ReactGoogleReviews
+          className="reviews"
+          layout="carousel"
+          theme="dark"
+          reviewVariant="testimonial"
+          logoVariant="none"
+          featurableId={featurableWidgetId}
+        />
       </div>
     </section>
-      <div className="reviews"> 
-        
-      <ReactGoogleReviews className="reviews"layout="carousel" theme="dark" reviewVariant="testimonial" logoVariant="none"  featurableId={featurableWidgetId} />
-      
-  
-    </div>
-    
-      </section>
-
   );
 };
 
